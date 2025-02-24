13c295403cb58cd21f69bab7d31a681a8af079b4
dkeenan <keenan.deboissy@epitech.eu>
2025-02-24 17:07:56 +0100

    [ADD] domain extraction function to improve ping handling and error management

--- Bot/Status/ping.js
+++ Bot/Status/ping.js
@@ -1,13 +1,30 @@
 const { exec } = require("child_process");
 
+function extractDomain(url) {
+    try {
+        let hostname = new URL(url).hostname;
+        return (hostname);
+    } catch (error) {
+        return (null);
+    }
+}
+
 async function ping(domain) {
+    let domainName = extractDomain(domain);
+
+    if (domainName === null)
+        return -1;
+
     return new Promise((resolve, reject) => {
-        exec(`ping -c 1 ${domain} | awk -F'/' 'END {print $5}'`, (error, stdout, stderr) => {
-            if (error) {
-                reject(`Erreur: ${stderr || error.message}`);
-                return;
-            }
-            resolve(parseFloat(stdout.trim()) + " ms");
+        exec(`ping -c 1 ${domainName} | awk -F'/' 'END {print $5}'`, (error, stdout, stderr) => {
+            if (error)
+                return resolve(-1)
+
+            const stdoutValue = stdout.trim()
+            const result = parseFloat(stdoutValue);
+            if (isNaN(result))
+                return resolve(-1);
+            resolve(result);
         });
     });
 }
